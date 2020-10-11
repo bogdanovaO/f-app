477641c5cabf0ef27ac65c0c08f9939a54ab4fd2
bogdanovaO <olya@MacBook-Pro-Olya.local>
2020-10-11 22:02:37 +0300

    Updates

---
+++ precache-manifest.980ea9491ca9cbd6e470cc7d91228628.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14407f0f8ba2359961ea7f4e6e5b379e",
+    "url": "/f-app/index.html"
+  },
+  {
+    "revision": "4e0b4e7326ababc72302",
+    "url": "/f-app/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "1bc47972e885dbc5b9bf",
+    "url": "/f-app/static/css/main.3507d01a.chunk.css"
+  },
+  {
+    "revision": "4e0b4e7326ababc72302",
+    "url": "/f-app/static/js/2.2b83508c.chunk.js"
+  },
+  {
+    "revision": "b5d0d730c356ed1240d161f4c511886b",
+    "url": "/f-app/static/js/2.2b83508c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc47972e885dbc5b9bf",
+    "url": "/f-app/static/js/main.84f4d8ce.chunk.js"
+  },
+  {
+    "revision": "1f4d9f445e5073430e5b",
+    "url": "/f-app/static/js/runtime-main.d37ebeb5.js"
+  },
+  {
+    "revision": "4294caa4ec73b6246083daa52f25e866",
+    "url": "/f-app/static/media/designBmw.4294caa4.jpg"
+  },
+  {
+    "revision": "7078534d63cfaf51d4dec4e9f035f6b8",
+    "url": "/f-app/static/media/designCandle.7078534d.jpg"
+  },
+  {
+    "revision": "72db807c7a8cfae39b4f636b385badd5",
+    "url": "/f-app/static/media/designIos.72db807c.jpg"
+  },
+  {
+    "revision": "c0ade567cc71ce228942b0592b73901f",
+    "url": "/f-app/static/media/gallery.c0ade567.png"
+  },
+  {
+    "revision": "60a2b252daf46f51a209fdda76ad3e0e",
+    "url": "/f-app/static/media/galleryDetail.60a2b252.png"
+  },
+  {
+    "revision": "754fb0f2b6546362a753b53edf2c48e4",
+    "url": "/f-app/static/media/img.754fb0f2.jpg"
+  },
+  {
+    "revision": "7b67ddc686397609febd4be9e8765ade",
+    "url": "/f-app/static/media/mainPage.7b67ddc6.jpg"
+  },
+  {
+    "revision": "c1249c9ac5129ff927c0a64ee8c91b95",
+    "url": "/f-app/static/media/profilePic.c1249c9a.svg"
+  }
+]);
